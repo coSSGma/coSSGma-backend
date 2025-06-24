f2bf00e4fb2f149e61657f0090a63a95d209cb15
haeyoon1 <hynam1220@naver.com>
2025-06-24 15:18:09 +0900

    feat: Initial Spring Boot project from Spring Initializr

---
+++ src/test/java/com/example/coSSGma/CoSsGmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coSSGma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoSsGmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
