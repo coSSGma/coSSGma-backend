746bbd6a47c78afe617ef551eb718e920e607e7a
haeyoon1 <hynam1220@naver.com>
2025-06-25 17:31:53 +0900

    feat: 어노테이션 추가

--- src/main/java/com/example/coSSGma/db/repository/GroupRepository.java
+++ src/main/java/com/example/coSSGma/db/repository/GroupRepository.java
@@ -2,6 +2,8 @@ package com.example.coSSGma.db.repository;
 
 import com.example.coSSGma.db.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 }
